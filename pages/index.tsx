7a6a361370e684f22e0f67336c9c1e647fd312d6
Tej Mehta <tmehta@horizontal.com>
2023-03-26 21:26:51 +0530

    added h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>Hello Frnds</h1>
       <Constructions />
       <Services />
       <ContactForm />
